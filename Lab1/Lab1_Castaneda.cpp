8633dad74175910d46a47f4774835616d0526a73
theZAZA1 <julkyng@gmail.com>
2026-06-02 01:27:25 +0000

    complete lab 1

--- Lab1/Lab1_Castaneda.cpp
+++ Lab1/Lab1_Castaneda.cpp
@@ -6,7 +6,53 @@ lab 1: iostream
 
 #include<iostream>
 
+using namespace std;
+
 int main(){
-    std::cout<<"Hello World!";
-    return 0;
+    cout<<"\n ------ Example 1: Cout object ------"<<endl;
+    cout<<"Hello World!"<<endl;
+    cout<<"ET575 \t Student's full name"<<endl;
+    cout<<"\n ------ Example 2: Declaring string and character variables ------"<<endl;
+
+    /*
+    Variables are onlt declared once
+    - Naming variables:
+        - Can't start with a number
+        - Can't have space in between
+        - Can't have any symbol except the _
+    A virable name is known as a "Identifier"
+
+    */
+   string username;
+   username = "Peter Pan";
+   cout<<"Username = "<<username<<endl;
+   cout<<"Enter another username: ";
+   cin>>username;
+   cout<<"Updated username: "<<username<<endl;
+   cout<<"\n ------ Example 3: Declaring character variables ------"<<endl;
+   char symbol = '%';
+   cout<<"Character: "<<symbol<<endl;
+   symbol = 38;
+   cout<<"Updated character: "<<symbol<<endl;
+
+   cout<<"------ EXCERCISE ------"<<endl;
+
+   string country;
+    cout<<"Enter a Country: ";
+    cin>> country;
+
+    char gender;
+    cout<<"Enter a gender (F for female, M for male, O for other): ";
+    cin>> gender;
+
+    cout<< "\nEntered country:\t"<<country<<endl;
+    cout<< "Selected gender\t"<<gender<<endl;
+
+
+
+   return 0;
+
+
+
+
 }
